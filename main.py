f4a5a91638696ba58e402815f49e77d3bff2b2e7
Lars Olt <larsolt1@gmail.com>
2024-06-13 13:38:41 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,230 @@
+import xlrd
+import csv
+from decimal import Decimal
+
+from relab_preprocess_vars import (
+    material_classes, meteorites, sediments, organics, minerals, rocks, 
+    synthetics, minerals_o, mixtures_o, organics_o, rocks_o, sediments_o
+)
+
+sample_cat_dir = "./RelabDatabase2023Dec31/catalogues/Sample_Catalogue.xls"
+spectra_cat_dir = "./RelabDatabase2023Dec31/catalogues/Spectra_Catalogue.xls"
+chem_analyses_dir = "./RelabDatabase2023Dec31/catalogues/Chem_Analyses.xls"
+data_dir = "./RelabDatabase2023Dec31/data/"
+
+
+# classifies the material class of sample based on metadata
+def get_material_class(id, source, genType1, gen_type_2, type1, subtype):
+
+    # hard coded id classifications
+    match id:
+        case 'DH-CMP-026': return 'Reference'
+        case 'DH-CMP-027': return 'Rock'
+        case 'DH-CMP-028': return 'Rock'
+        case 'SS-CMP-001': return 'Rock'
+        case 'SS-CMP-002': return 'Rock'
+        case 'SS-CMP-003': return 'Rock'
+        case 'SS-CMP-004': return 'Rock'
+        case 'TT-CMP-001': return 'Synthetic'
+    
+    if 'sediment' in genType1.lower(): return 'Mixture'
+    if 'standard' in type1.lower(): return 'Reference'
+    if 'synthetic' in source.lower(): return 'Synthetic'
+    if 'biological' in genType1.lower(): return 'Organic'
+        
+    if source in meteorites: return 'Meteorites'
+
+    if 'moon-ret' in source.lower(): return 'Returned Planetary Samples'
+    if 'mixture' in type1.lower(): return 'Mixture'
+    if 'dune sand' in type1.lower(): return 'Sediment'
+
+    if genType1 in material_classes: return genType1
+
+    if 'mixture' in gen_type_2.lower(): return "Mixture"
+    
+    if genType1 in sediments: return "Sediment"
+    if genType1 in organics: return "Organic"
+    if genType1 in minerals: return "Mineral"
+    if genType1 in rocks: return "Rock"
+    if genType1 in synthetics: return "Synthetic"
+
+    if 'quartz kbr' in subtype.lower(): return 'Rock'
+
+    # outlier (hard coded) classifications
+    sample_attributes = f'{genType1}, {type1}, {subtype}'
+    if sample_attributes in minerals_o: return 'Mineral'
+    if sample_attributes in mixtures_o: return 'Mixture'
+    if sample_attributes in organics_o: return 'Organic'
+    if sample_attributes in rocks_o: return 'Rock'
+    if sample_attributes in sediments_o: return 'Sediment'
+
+    # default case
+    return None
+
+
+def load_sample_data():
+    # read sample category excel file
+    sample_cat = xlrd.open_workbook(sample_cat_dir).sheet_by_index(0)
+    
+    sample_data = {}
+    
+    # apply algorithm to all samples, store metadata
+    for sample in range(1, sample_cat.nrows):
+
+        # retrive sample metadata
+        sample_id      = sample_cat.cell_value(rowx=sample, colx=0)
+        sample_name    = sample_cat.cell_value(rowx=sample, colx=1)
+        source         = sample_cat.cell_value(rowx=sample, colx=5)
+        gen_type_1     = sample_cat.cell_value(rowx=sample, colx=6)
+        gen_type_2     = sample_cat.cell_value(rowx=sample, colx=7)
+        type_1         = sample_cat.cell_value(rowx=sample, colx=8)
+        sub_type       = sample_cat.cell_value(rowx=sample, colx=10)
+        min_grain_size = sample_cat.cell_value(rowx=sample, colx=12)
+        max_grain_size = sample_cat.cell_value(rowx=sample, colx=13)
+        # location       = sample_cat.cell_value(rowx=sample, colx=17)
+        location       = 'ReLab'
+        chem_num       = sample_cat.cell_value(rowx=sample, colx=18)
+        text           = sample_cat.cell_value(rowx=sample, colx=19)
+
+        # classify material class
+        m_class = get_material_class(sample_id, source, gen_type_1, gen_type_2, type_1, sub_type)
+
+        if m_class:
+            sample_data[sample_id] = {
+                "chem_number": str(int(chem_num)) if chem_num != "" else "",
+                "max_grain_size": str(max_grain_size),
+                "min_grain_size": str(min_grain_size),
+                "Location": location,
+                "sample_name": sample_name,
+                "MaterialClass": gen_type_1,
+                "SampleDescription": text
+            }
+        
+        else:
+            sample_str = f'{gen_type_1}, {type_1}, {sub_type}'
+            print(f'Skipping {sample_str}. Could not classify.')
+    
+    print("")
+    return sample_data
+
+
+def load_chem_analysis():
+    chemCat = xlrd.open_workbook(chem_analyses_dir).sheet_by_index(0)
+
+    chem_analysis = {}
+
+    # store relevant data from chem catalogue under reference number 
+    for val in range(1, chemCat.nrows):
+        
+        ref_num = str(int(chemCat.cell_value(rowx=val, colx=0)))
+        other_info = str(chemCat.cell_value(rowx=val, colx=22)).strip()
+        references = str(chemCat.cell_value(rowx=val, colx=21)).strip()
+
+        chem_analysis[ref_num] = {"OtherInfo": other_info, "References": references}
+    
+    return chem_analysis
+
+
+if __name__ == "__main__":
+    
+    output_cnt = 0
+    
+    sample_data = load_sample_data()
+    chem_analysis = load_chem_analysis()
+    
+    spectra_cat = xlrd.open_workbook(spectra_cat_dir).sheet_by_index(0)
+    
+    # get relevant data from specta catalogue under sample_id 
+    for spectra in range(1, spectra_cat.nrows):
+        
+        spectra_data = {}
+
+        # get spectra info
+        spectra_id = spectra_cat.cell_value(rowx=spectra, colx=0)
+        min_wavelength = int(spectra_cat.cell_value(rowx=spectra, colx=5))
+        max_wavelength = int(spectra_cat.cell_value(rowx=spectra, colx=6))
+        resolution = spectra_cat.cell_value(rowx=spectra, colx=7)
+        date_added = spectra_cat.cell_value(rowx=spectra, colx=2)
+        source = spectra_cat.cell_value(rowx=spectra, colx=8)
+        detect = spectra_cat.cell_value(rowx=spectra, colx=9)
+        
+        date_added = xlrd.xldate_as_datetime(date_added, 1).date().isoformat()
+        view_geo = f'{source}° / {detect}°' if (source!="NA" and detect!="NA") else ""
+
+        # get sample info
+        sample_id = spectra_cat.cell_value(rowx=spectra, colx=1)
+        
+        try:
+            spectra_sample_data = sample_data[sample_id]
+        except:
+            # skip spectra whose id is not classified
+            continue
+        
+        # skip spectra whose material class was not classified
+        if (spectra_sample_data['MaterialClass'] == ''): continue
+
+        # get chem analysis info
+        chem_num = spectra_sample_data["chem_number"]
+        other_info = chem_analysis[chem_num]["OtherInfo"] if (chem_num!="0" and chem_num!='') else ""
+        refs = chem_analysis[chem_num]["References"] if (chem_num!="0" and chem_num!='') else ""
+
+        header = [
+            ["Composition", ""],
+            ["Date Added", date_added],
+            ["Formula", ""],
+            ["Grain Size Description", f'<{spectra_sample_data["max_grain_size"]}um'],
+            ["Grain Size", f'{spectra_sample_data["min_grain_size"]} - {spectra_sample_data["max_grain_size"]}'],
+            ["Locality", ""],
+            ["Minimum Wavelength", min_wavelength],
+            ["Sample Name", spectra_sample_data["sample_name"]],
+            ["Maximum Wavelength", max_wavelength],
+            ["Database of Origin", spectra_sample_data["Location"]],
+            ["Other Information", other_info],
+            ["References", refs],
+            ["Resolution", resolution],
+            ["Material Class", spectra_sample_data["MaterialClass"]],
+            ["Sample Description", spectra_sample_data["SampleDescription"]],
+            ["Sample ID", spectra_id],
+            ["Original Sample ID", sample_id],
+            ["Viewing Geometry", view_geo],
+            ["Wavelength", "Response"]
+        ]
+        
+        # compute location of spectral data
+        pi_initials = sample_id.split("-")[1].lower()
+        sub_folder_name = sample_id.split("-")[0].lower()
+        file_path = f'{data_dir}{pi_initials}/{sub_folder_name}/{spectra_id.lower()}.txt'
+        
+        try:
+            # read spectral data
+            lines = open(file_path).read().splitlines()
+            lines = lines[2:] # remove first two lines
+        except:
+            print(f'Could not find directory {file_path}')
+            continue
+
+        try:
+            # convert data from microns to nms
+            MICRON_TO_NM = 1000
+            data = [
+                [
+                    str(float( Decimal(line.split("\t")[0]) * MICRON_TO_NM )),
+                    str(float( Decimal(line.split("\t")[1]) * MICRON_TO_NM ))
+                ]
+                for line in lines
+            ]
+        except:
+            print(f'Error parsing data at {file_path}')
+            continue
+
+        output_dest = f'output/{pi_initials}_{sub_folder_name}_{spectra_id.lower()}.csv'
+        with open(output_dest, 'w', newline='', encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(header)
+            writer.writerows(data)
+        
+        print(f'Successfully wrote {output_dest}')
+        output_cnt+=1
+    
+    print("")
+    print(f'Wrote {output_cnt} files.')
---
+++ relab_preprocess_vars.py
@@ -0,0 +1,176 @@
+# attributes of database to classify into
+material_classes = {
+    "Meteorites", "Mineral", "Mixture", "Organic", "Spacecraft Observations", 
+    "Reference", "Rock", "Sediment", "Synthetic", "Volatiles", 
+    "Returned Planetary Samples"
+}
+
+# defined classifications  
+meteorites = {
+    'Mars-Met', 'Moon-Met', 'Other-Ast', 'Other-Qet', 'Other-Vet'
+}
+
+sediments = {
+    'Sand', 'Ash', 'Regolith'
+}
+
+organics = {
+    'Biological', 'Organic'
+}
+
+minerals = {
+    'Element', 'MineralPwdr', 'Coating'
+}
+
+rocks = {
+    'Glass', 'PockPowder', 'RockCoating', 'RockFines', 'Rocks', 
+    'RocksSlab', 'Spherule', 'Tektite'
+}
+
+synthetics = {
+    'Polymer', 'Reagent'
+}
+
+# outlier classifications
+# format: generalType1, type1, subtype
+minerals_o = {
+    ', , Albertite ',
+    ', , Amersil (BC) Qtz',
+    ', , Anatase ',
+    ', , Antrharxolite ',
+    ', , Fe2O3 ',
+    ', , Nacholite ',
+    'Mineral, Bromide, KBr Potassium Bromide',
+    'Mineral, Bromide, Potassium Bromide KBr',
+    'Mineral, Carbonate , Anhydrous Oxalate Acid',
+    'Mineral, Carbonate , Ankerite',
+    'Mineral, Carbonate , Aragonite ',
+    'Mineral, Carbonate , Artinite',
+    'Mineral, Carbonate , Azurite',
+    'Mineral, Carbonate , Brugnatellite ',
+    'Mineral, Carbonate , Calcite ',
+    'Mineral, Carbonate , Cerussite',
+    'Mineral, Carbonate , Coalingite',
+    'Mineral, Carbonate , Dawsonite ',
+    'Mineral, Carbonate , Dolomite',
+    'Mineral, Carbonate , Gaspeite',
+    'Mineral, Carbonate , Gaylussite',
+    'Mineral, Carbonate , Heated Gaylussite ',
+    'Mineral, Carbonate , Heated Hydromagnesite ',
+    'Mineral, Carbonate , Heated Hydrotalcite ',
+    'Mineral, Carbonate , Heated Manasseite ',
+    'Mineral, Carbonate , Heated Pirssonite ',
+    'Mineral, Carbonate , Heated Thermonatrite',
+    'Mineral, Carbonate , Heated Trona',
+    'Mineral, Carbonate , Huntite ',
+    'Mineral, Carbonate , Hydrocalcite',
+    'Mineral, Carbonate , Hydrocerussite',
+    'Mineral, Carbonate , Hydromagnesite',
+    'Mineral, Carbonate , Hydrotalcite',
+    'Mineral, Carbonate , Magnesite',
+    'Mineral, Carbonate , Magnesite ',
+    'Mineral, Carbonate , Malachite',
+    'Mineral, Carbonate , Manasseite',
+    'Mineral, Carbonate , Manganocalcite',
+    'Mineral, Carbonate , Monohydrocalcite',
+    'Mineral, Carbonate , Nahcolite ',
+    'Mineral, Carbonate , Northupite',
+    'Mineral, Carbonate , Pirssonite',
+    'Mineral, Carbonate , Pyroaurite',
+    'Mineral, Carbonate , Rhodochrosite',
+    'Mineral, Carbonate , Shortite',
+    'Mineral, Carbonate , Siderite',
+    'Mineral, Carbonate , Sodium Carbonate',
+    'Mineral, Carbonate , Sodium Oxalate',
+    'Mineral, Carbonate , Thermonatrite ',
+    'Mineral, Carbonate , Trona ',
+    'Mineral, Carbonate, Artinite',
+    'Mineral, Carbonate, Bruganatellite',
+    'Mineral, Carbonate, Coalingite',
+    'Mineral, Carbonate, Dypingite Yoshikawaite',
+    'Mineral, Carbonate, Gaylussite',
+    'Mineral, Carbonate, Hydromagnesite',
+    'Mineral, Carbonate, Magnesite',
+    'Mineral, Carbonate, Pirssonite',
+    'Mineral, Carbonate, Pokrovskite',
+    'Mineral, Carbonate, Sjogrenite',
+    'Mineral, Carbonate, Thermonatrite ',
+    'Mineral, Carbonate, Trona ',
+    'Mineral, Chloride, Ammonium Chloride (NH4)Cl ',
+    'Mineral, Chloride, Atacamite',
+    'Mineral, Chloride, Paratacamite',
+    'Mineral, Element, Iron',
+    'Mineral, Glass, Basaltic Volcanic Glass',
+    'Mineral, Glass, Rhyolitic Volcanic Glass',
+    'Mineral, Hydroxide , Brucite ',
+    'Mineral, Melt, Melt Inclusion',
+    'Mineral, Oxide , Corundum',
+    'Mineral, Oxide , Hematite',
+    'Mineral, Oxide, Alexandrite',
+    'Mineral, Phosphate, Apatite',
+    'Mineral, Silicate (Ino) , Pyroxene Orthopyroxene Enstatite Reduced',
+    'Mineral, Silicate (Ino) , Pyroxene Shocked',
+    'Mineral, Silicate (Ino), Orthopyroxene',
+    'Mineral, Silicate (Ino), Pyroxene',
+    'Mineral, Silicate (Ino), Pyroxene Clinopyroxene Augite',
+    'Mineral, Silicate (Ino), Pyroxene Clinopyroxene Diopside',
+    'Mineral, Silicate (Ino), Pyroxene Orthopyroxene Hypersthene',
+    'Mineral, Silicate (Neso), Olivine',
+    'Mineral, Silicate (Neso), Olivine Forsterite',
+    'Mineral, Silicate (Neso), Olivine Heated',
+    'Mineral, Silicate (Neso), Olivine Shocked ',
+    'Mineral, Silicate (Neso), OlivineForsterite ',
+    'Mineral, Silicate (Ortho), Forsterite ',
+    'Mineral, Silicate (Phyllo) , Berthierine ',
+    'Mineral, Silicate (Phyllo) , Chamosite ',
+    'Mineral, Silicate (Phyllo) , Chlorite',
+    'Mineral, Silicate (Phyllo) , Heated Chlorite ',
+    'Mineral, Silicate (Phyllo) , Heated Serpentine Antigorite',
+    'Mineral, Silicate (Phyllo) , Kaolinite',
+    'Mineral, Silicate (Phyllo) , Nontronite',
+    'Mineral, Silicate (Phyllo) , Serpentine Antigorite ',
+    'Mineral, Silicate (Phyllo) , Smectite',
+    'Mineral, Silicate (Phyllo), Allophane',
+    'Mineral, Silicate (Phyllo), Beidellite',
+    'Mineral, Silicate (Tecto), Anorthite ',
+    'Mineral, Silicate (Tecto), Feldspar Plagioclase Anorthite',
+    'Mineral, Silicate (Tecto), Feldspar Plagioclase Bytownite',
+    'Mineral, Silicate (Tecto), Feldspar Plagioclase Labradorite',
+    'Mineral, Silicate (Tecto), Laser-Irradiated Anorthite',
+    'Mineral, Silicate (Tecto), Natrolite',
+    'Mineral, Silicate (Tecto), Plagioclase Feldspar',
+    'Mineral, Silicate (Tecto), Quartz',
+    'Mineral, Silicate (Tecto), Zeolite',
+    'Mineral, Sulfate , Alunite ',
+    'Mineral, Sulfate , Anhydrite ',
+    'Mineral, Sulfate , Calcium Sulfate Dihydrate Gypsum',
+    'Mineral, Sulfate , Copiapite ',
+    'Mineral, Sulfate , Gypsum',
+    'Mineral, Sulfate , Jarosite',
+    'Mineral, Sulfate , Kieserite ',
+    'Mineral, Sulfate , Magnesium Sulfate ',
+    'Mineral, Sulfate , Water Dissolved Copiapite ',
+    'Mineral, Sulfate, Gypsum',
+    'Mineral, Sulfide , Chalcopyrite',
+    'Mineral, Sulfide , Pentlandite ',
+    'Mineral, , ',
+    'Mineral, Glass , ',
+    'Mineral, Iron Oxide, '
+}
+
+mixtures_o = {
+    'Mineral, Mixture , Hypersthene Augite',
+    'Mineral, Mixture'
+}
+
+organics_o = {
+    ', Organic , Asphaltite'
+}
+
+rocks_o = {
+    'RockSlab, Sedimentary , Sandstone '
+}
+
+sediments_o = {
+    'Soil, Volcanic, Volcanic Alteration'
+}
