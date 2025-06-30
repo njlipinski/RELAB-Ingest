ab8e08dbdedcb9e2a0859f702b36d18cc1eca57b
njlipinski <njlipinski1@gmail.com>
2025-06-30 15:53:40 -0700

    Update main.py
    Changed material class --> sample type to use in VISOR
    Reformatted Grain Size for consistency
    changed ReLab --> RELAB

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ from relab_preprocess_vars import (
     synthetics, minerals_o, mixtures_o, organics_o, rocks_o, sediments_o
 )
 
-sample_cat_dir = "./RelabDatabase2023Dec31/catalogues/Sample_Catalogue.xls"
-spectra_cat_dir = "./RelabDatabase2023Dec31/catalogues/Spectra_Catalogue.xls"
-chem_analyses_dir = "./RelabDatabase2023Dec31/catalogues/Chem_Analyses.xls"
-data_dir = "./RelabDatabase2023Dec31/data/"
+sample_cat_dir = "../RelabDatabase2023Dec31/catalogues/Sample_Catalogue.xls"
+spectra_cat_dir = "../RelabDatabase2023Dec31/catalogues/Spectra_Catalogue.xls"
+chem_analyses_dir = "../RelabDatabase2023Dec31/catalogues/Chem_Analyses.xls"
+data_dir = "../RelabDatabase2023Dec31/data/"
 
 
 # classifies the material class of sample based on metadata
-def get_material_class(id, source, genType1, gen_type_2, type1, subtype):
+# changed to "Sample type" -- VISOR has fields for both but Sample type is used for consistency
+def get_sample_type(id, source, genType1, gen_type_2, type1, type2, subtype):
 
     # hard coded id classifications
     match id:
@@ -78,16 +79,17 @@ def load_sample_data():
         gen_type_1     = sample_cat.cell_value(rowx=sample, colx=6)
         gen_type_2     = sample_cat.cell_value(rowx=sample, colx=7)
         type_1         = sample_cat.cell_value(rowx=sample, colx=8)
+        type_2         = sample_cat.cell_value(rowx=sample, colx=9)
         sub_type       = sample_cat.cell_value(rowx=sample, colx=10)
         min_grain_size = sample_cat.cell_value(rowx=sample, colx=12)
         max_grain_size = sample_cat.cell_value(rowx=sample, colx=13)
         # location       = sample_cat.cell_value(rowx=sample, colx=17)
-        location       = 'ReLab'
+        location       = 'RELAB' # Changed from ReLab
         chem_num       = sample_cat.cell_value(rowx=sample, colx=18)
         text           = sample_cat.cell_value(rowx=sample, colx=19)
 
         # classify material class
-        m_class = get_material_class(sample_id, source, gen_type_1, gen_type_2, type_1, sub_type)
+        m_class = get_sample_type(sample_id, source, gen_type_1, gen_type_2, type_1, type_2, sub_type)
 
         if m_class:
             sample_data[sample_id] = {
@@ -145,11 +147,11 @@ if __name__ == "__main__":
         max_wavelength = int(spectra_cat.cell_value(rowx=spectra, colx=6))
         resolution = spectra_cat.cell_value(rowx=spectra, colx=7)
         date_added = spectra_cat.cell_value(rowx=spectra, colx=2)
-        source = spectra_cat.cell_value(rowx=spectra, colx=8)
-        detect = spectra_cat.cell_value(rowx=spectra, colx=9)
+        source_angle = spectra_cat.cell_value(rowx=spectra, colx=8)
+        detect_angle = spectra_cat.cell_value(rowx=spectra, colx=9)
         
         date_added = xlrd.xldate_as_datetime(date_added, 1).date().isoformat()
-        view_geo = f'{source}° / {detect}°' if (source!="NA" and detect!="NA") else ""
+        view_geo = f'{source_angle}° / {detect_angle}°' if (source_angle!="NA" and detect_angle!="NA") else ""
 
         # get sample info
         sample_id = spectra_cat.cell_value(rowx=spectra, colx=1)
@@ -169,25 +171,27 @@ if __name__ == "__main__":
         refs = chem_analysis[chem_num]["References"] if (chem_num!="0" and chem_num!='') else ""
 
         header = [
-            ["Composition", ""],
-            ["Date Added", date_added],
+	    ["Sample ID", spectra_id],
+	    ["Sample Name", spectra_sample_data["sample_name"]],
+	    ["Material class", ""],
+	    ["Sample type", spectra_sample_data["MaterialClass"]], # Changed from "Material Class"
+	    ["Date Added", date_added],
             ["Formula", ""],
-            ["Grain Size Description", f'<{spectra_sample_data["max_grain_size"]}um'],
-            ["Grain Size", f'{spectra_sample_data["min_grain_size"]} - {spectra_sample_data["max_grain_size"]}'],
-            ["Locality", ""],
-            ["Minimum Wavelength", min_wavelength],
-            ["Sample Name", spectra_sample_data["sample_name"]],
-            ["Maximum Wavelength", max_wavelength],
-            ["Database of Origin", spectra_sample_data["Location"]],
-            ["Other Information", other_info],
-            ["References", refs],
-            ["Resolution", resolution],
-            ["Material Class", spectra_sample_data["MaterialClass"]],
             ["Sample Description", spectra_sample_data["SampleDescription"]],
-            ["Sample ID", spectra_id],
+            ["Database of Origin", spectra_sample_data["Location"]],
+	    ["Locality", ""],
+            ["Grain Size Description", f'<{spectra_sample_data["max_grain_size"]}um'],
+            ["Grain Size", f'({spectra_sample_data["min_grain_size"]}_ {spectra_sample_data["max_grain_size"]})'], #Edited for consistency
+	    ["Viewing Geometry", view_geo],
+            ["Resolution", resolution],
+            ["Wavelength", "Response"],
+            ["Minimum Wavelength", min_wavelength],
+            ["Maximum Wavelength", max_wavelength],
+	    ["Composition", ""],
+            ["References", refs],
             ["Original Sample ID", sample_id],
-            ["Viewing Geometry", view_geo],
-            ["Wavelength", "Response"]
+            ["Other Information", other_info],
+	    ["Image", ""]
         ]
         
         # compute location of spectral data
@@ -209,7 +213,7 @@ if __name__ == "__main__":
             data = [
                 [
                     str(float( Decimal(line.split("\t")[0]) * MICRON_TO_NM )),
-                    str(float( Decimal(line.split("\t")[1]) * MICRON_TO_NM ))
+                    #str(float( Decimal(line.split("\t")[1]) * MICRON_TO_NM )) # <-- Reflectance should be 0..1 (not multiplied by 1000)
                 ]
                 for line in lines
             ]
@@ -217,7 +221,7 @@ if __name__ == "__main__":
             print(f'Error parsing data at {file_path}')
             continue
 
-        output_dest = f'output/{pi_initials}_{sub_folder_name}_{spectra_id.lower()}.csv'
+        output_dest = f'output/{sub_folder_name}_{pi_initials}_{spectra_id.lower()}.csv'
         with open(output_dest, 'w', newline='', encoding="utf-8") as csvfile:
             writer = csv.writer(csvfile)
             writer.writerows(header)
